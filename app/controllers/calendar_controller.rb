c91437a1024a682808384da54d614c47e6bc0478
Steven Ritchie <sritchie.ivey@gmail.com>
2013-03-31 11:27:09 -0600

    Tomorrow button

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,6 +3,7 @@ class CalendarController < ApplicationController
 	def index
 		@events = Event.all
 		@date = params[:month] ? Date.parse(params[:month].gsub('-', '/')) : Date.today
+		@eventDate = params[:day] ? Date.parse(params[:day].gsub('-', '/')) : Time.now.to_date
 	end
 
 	def faq
