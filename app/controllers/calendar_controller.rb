d025293a6dc697e300f8b0a8e1a27f3584b8236d
Steven Ritchie <sritchie.ivey@gmail.com>
2013-03-31 19:14:11 -0600

    Conditional Month Colours

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,7 +2,7 @@ class CalendarController < ApplicationController
 
 	def index
 		@events = Event.all
-		@date = params[:month] ? Date.parse(params[:month].gsub('-', '/')) : Date.today
+		@date = params[:month] ? Date.parse(params[:month].gsub('-', '/')) : Time.now.to_date
 		@eventDate = params[:day] ? Date.parse(params[:day].gsub('-', '/')) : Time.now.to_date
 	end
 
