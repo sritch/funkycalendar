26bb5fa15f611c78790f719fb740a6180a4c2f11
Steven Ritchie <sritchie.ivey@gmail.com>
2013-05-29 22:00:33 -0600

    fixed model

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,22 +1,3 @@
 class Day < ActiveRecord::Base
   attr_accessible :day_date, :title
-
- # def current_month(month)
-/		return 'january' if month == 'January'
-		return 'february' if month == 'February'
-		return 'march' if month == 'March'
-		return 'april' if month == 'April'
-		return 'may' if month == 'May'
-		return 'june' if month == 'June'
-		return 'july' if month == 'July'
-		return 'august' if month == 'August'
-		return 'september' if month == 'September'
-		return 'october' if month == 'October'
-		return 'november' if month == 'November'
-		return 'december' if month == 'December'
-	end
-
-	def month_class(month)
-		"#{current_month(month)}"
-	end/
 end
